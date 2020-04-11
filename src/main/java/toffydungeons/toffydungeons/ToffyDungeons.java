2766f2c592c06a54a79de9742b50432e844f4cd7
CalebGitBritannia <calebbritannia@gmail.com>
2020-04-11 21:07:52 +0100

    Added java folders and files, plugin is ready to be programmed.

---
+++ src/main/java/toffydungeons/toffydungeons/ToffyDungeons.java
@@ -0,0 +1,17 @@
+package toffydungeons.toffydungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ToffyDungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("[Toffy Dungeons] Toffy Dungeons version 1.0 loaded up! ");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
