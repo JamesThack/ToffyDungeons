7bcec3b850ba070c57f450ad9c47055037a2df66
CalebGitBritannia <calebbritannia@gmail.com>
2020-04-11 21:17:16 +0100

    Checking that discord integration works!

--- src/main/java/toffydungeons/toffydungeons/ToffyDungeons.java
+++ src/main/java/toffydungeons/toffydungeons/ToffyDungeons.java
@@ -7,6 +7,7 @@ public final class ToffyDungeons extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("[Toffy Dungeons] Toffy Dungeons version 1.0 loaded up! ");
+        System.out.println("Hello Discord!");
 
     }
 
