1a2adfb1a3cad3454268c027139acf93e71d89f3
CalebGitBritannia <calebbritannia@gmail.com>
2020-04-11 22:26:13 +0100

    Added world edit support

--- src/main/java/toffydungeons/toffydungeons/ToffyDungeons.java
+++ src/main/java/toffydungeons/toffydungeons/ToffyDungeons.java
@@ -1,13 +1,19 @@
 package toffydungeons.toffydungeons;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ToffyDungeons extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("[Toffy Dungeons] Toffy Dungeons version 1.0 loaded up! ");
-        System.out.println("Hello Discord!");
+        if (Bukkit.getPluginManager().getPlugin("WorldEdit") == null) {
+            System.out.println("[Toffy Dungeons] §cFATAL ERROR: WORLD EDIT NOT INSTALLED, PLEASE INSTALL WORLD EDIT TO USE THE PLUGIN");
+        } else {
+            System.out.println("[Toffy Dungeons] Toffy Dungeons version " + this.getServer().getVersion() +" loaded up! ");
+        }
+
 
     }
 
